4403f28835d787da5f3087ce32619829c1da852b
Kevin Salim <nivek.salim@gmail.com>
2020-05-13 11:39:50 +0200

    Chart

--- ui/dashboard/src/app/app.component.ts
+++ ui/dashboard/src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import * as dc from "dc";
+import { HttpClient } from "@angular/common/http";
+import * as crossfilter from "crossfilter";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
-  title = 'dashboard';
+export class AppComponent implements OnInit {
+  countryChart: dc.RowChart;
+  data;
+  all;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http
+      .post("http://localhost:8888/druid/v2/sql", {
+        query: "SELECT * FROM archive"
+      })
+      .subscribe((result: any) => {
+        this.data = crossfilter(result);
+        this.all = this.data.groupAll();
+        this.drawCharts();
+      });
+  }
+
+  resetSettlementChart(): void {}
+
+  drawCharts(): void {
+    this.countryChart = dc.rowChart("#countryChart");
+    const countryDim = this.data.dimension(d => {
+      return d.Country;
+    });
+    const countryGroup = countryDim.group();
+    this.countryChart
+      .dimension(countryDim)
+      .group(countryGroup)
+      .elasticX(true);
+
+    dc.renderAll();
+  }
 }
